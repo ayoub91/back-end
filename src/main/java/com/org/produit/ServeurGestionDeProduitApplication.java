7df39dea4a858143b979c6e5693d5e62291d8cc1
ayoub <ayoub.boudad91@gmail.com>
2019-01-26 13:48:22 +0000

    first commit

---
+++ src/main/java/com/org/produit/ServeurGestionDeProduitApplication.java
@@ -0,0 +1,14 @@
+package com.org.produit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurGestionDeProduitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeurGestionDeProduitApplication.class, args);
+	}
+
+}
+
